909f0ac0f3b69b9d7f6d6a2e46908c15b7af479f
ignacioborraz <ignacioborraz@hotmail.com>
2022-12-26 12:26:53 -0300

    creacion de app de react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MINGA</h1>
+        <p>
+          app para realizar con el cohorte 40
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
