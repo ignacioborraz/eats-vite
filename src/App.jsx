ed229705204c172680fcad7e48a2c82847b6cbd5
ignacioborraz <ignacioborraz@hotmail.com>
2022-12-27 10:39:16 -0300

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,11 @@ import Layout from './layouts/Layout'
 import Carousel from './components/Carousel/Carousel';
 
 function App() {
-
-  let string = "App"
-  let numero = 1
-
   return (
-    <div className={string}>
-      <header className="App-header">
-        <h1>MINGA</h1>
-        <p>app para realizar con el cohorte 40</p>
-      </header>
       <Layout>
         <Carousel />
       </Layout>
-    </div>
   )
 }
 
-export default App;
+export default App
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,40 +1,28 @@
 import React, { useState } from 'react'
 
-const Header = () => {
+const Header = ({type,options}) => {
+  console.log(type)
 
-  // const {nombreDePropiedad} = objeto;
-  // const nombreDePropiedad = objeto.nombreDePropiedad;
+  const [nav, setNav] = useState(false)
 
-  const [variable, setVariable] = useState(false)
-
-  const handleMenu = () => {
-    // if(variable === true) {
-    //   setVariable(false)
-    // }
-
-    // if(variable === false) {
-    //   setVariable(true)
-    // }
-
-    setVariable(!variable)
-  }
+  const handleMenu = () => setNav(!nav)
 
   return (
-    <div>
-      <p onClick={handleMenu}>ICONO - Menu hamburguesa</p>
+    <>
+      {type==="nav" ?
+        <img onClick={handleMenu} class='header-button' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAYAAACoPemuAAAAAXNSR0IArs4c6QAAAI9JREFUWEftllEKwCAMQ+PJtqPsJttuPvyeFJ4otJCCf1HrMwabklZL2pfcGL0ZEzMxSoDqS3rslHTQk07o39GciNgj6Z7YiE4Z9lCysUtSH7urW+ZXJc2/m1S4volR/BEx5xh9lWkD1jlGr5I+pKV65xjF6RwLiPk/Ru3k/xglNtQ7xyhGEzMxSoDq03rsA2K0DCfODRjvAAAAAElFTkSuQmCC" /> :
+        <img onClick={handleMenu} class='header-button' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAYAAACoPemuAAAAAXNSR0IArs4c6QAAAb1JREFUWEftmIEtBFEQhr+rABWgAjpABagAHVABKkEFqAAVoAMlUAH5k13Z29t3O/PvuWwuJrmc5N7b+e5/82b+M2GkMRkpFysNtg8cAruV+p/AA/AI6G8rhii2DtwDAusKQZ1VkGk4F2wLeAUE1xeCu+1b1P7cBXsDdhLJDoDnxHqr+E+Bm0ySCkpw4XAUU2Gr2LOxDXxENzlgKuq1aILGutRxOmDfBpS2pC6BAzZaxXT1TwzVNjIN11FMDfUpCXYH6DaHwwHTwzM386saV+EbqQQumDq+GmZfkxWUFFZDToULVie5Ai4LGV+AcwdqiGJNFql3BGh+1qGjTqvUfOhQxVLHk1m8smAyh/PG03umdw05St0wNVe9N2uq75RUc3qpn4UiepSCkNUpudVQsspdXERcbQRMMLLQEbcaBdRY01AvRh+YM34WAjcPLOProzDtddeAmvRMzAPTyNlzMyb2dTrbEthfHmGbudN5lMBcz5UQ6nepjKe82lSUwGRRNp0s5p7jdgspgbm+3uRi5hJ0gWnM6Ff2MiMEtszCr798CEz9K+XPFyCtWtPUvxD6Ov8CcnqP+AfL6jZaxX4Av05CJyhsS4sAAAAASUVORK5CYII=" />
+      }
       {
-        variable
+        nav
           ? (
-            <div>
-              <a style={{ margin: '10px' }} href="">Home</a>
-              <a style={{ margin: '10px' }} href="">Comics</a>
-              <a style={{ margin: '10px' }} href="">About us</a>
+            <div className={`header-options-${type}`}>
+              {options.map(option => <a href="" className='header-options'>{option.name}</a> )}
             </div>
           )
           : null
       }
-
-    </div>
+    </>
   )
 }
 
